39670c6e4593e123d152587d0c6fa643c5457fef
entr0py43 <mugellorr@yahoo.com>
2018-03-05 14:40:36 -0500

    first

---
+++ settingupSite.R
@@ -0,0 +1,6 @@
+library(blogdown);
+
+# blogdown::new_site(theme = "halogenica/beautifulhugo", theme_example = TRUE)
+# 
+# install_theme("halogenica/beautifulhugo", theme_example = TRUE, update_config = TRUE)
+# install_theme("halogenica/beautifulhugo", force=TRUE)
